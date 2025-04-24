13fefc6a508b436f6c86ff29b6620fb5004178e1
SGaniyev <ganiyevsardor222@gmail.com>
2025-04-24 15:31:02 +0500

    Initial commit

---
+++ UniversityLibrarySystem/Infratstructure/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityLibrarySystem.Models;
+
+namespace UniversityLibrarySystem.Infratstructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Book> Books { get; set; }
+}
---
+++ UniversityLibrarySystem/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversityLibrarySystem.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public String Title { get; set; }
+    public String Author { get; set; }
+    public int Year { get; set; }
+    public String Genre { get; set; }
+}
